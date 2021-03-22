ee9a71f7e93ac6906168da42cd549c19b87c1c70
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-22 20:49:30 -0300

    Add Head in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        {/* Todo conteudo dentro do Head é jogado lá dentro do document */}
+        <title>Ínicio|ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  );
 }
