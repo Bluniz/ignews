0f6b4812d058971137024f470a375a69aa37b6df
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-22 20:45:19 -0300

    Add _document and Roboto font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>ig.news</title>
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main /> {/* Onde os dados serão renderizados */}
+          <NextScript />
+          {/* Onde o Next carregará os scripts da minha aplicação */}
+        </body>
+      </Html>
+    );
+  }
+}
