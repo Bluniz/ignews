ed414075cdc968ba1b60d68f88ded073d3feee2b
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-23 19:03:47 -0300

    Add HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
         {/* Todo conteudo dentro do Head é jogado lá dentro do document */}
-        <title>Ínicio|ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 mouth</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
