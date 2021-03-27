ca0b2e178f389060661a2864d48d23edb8a9d94f
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-27 16:43:19 -0300

    Add Link and prefetch in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import styles from "./styles.module.scss";
 import { SignInButton } from "../SignIn/index";
-
+import Link from "next/link";
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          {/* prefetch -> Quando terminar de carregar toda minha aplicação, o next
+            por baixo dos panos, deixou a página pré carregada para mim.
+          */}
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
