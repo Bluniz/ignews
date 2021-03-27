4e56b61482226b246affdbe2b132974338a99d10
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-27 16:54:10 -0300

    Add ActiveLink component with cloneElement functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,22 @@
 import styles from "./styles.module.scss";
 import { SignInButton } from "../SignIn/index";
 import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
+          <ActiveLink activeClassName={styles.active} href="/">
             <a className={styles.active}>Home</a>
-          </Link>
+          </ActiveLink>
           {/* prefetch -> Quando terminar de carregar toda minha aplicação, o next
             por baixo dos panos, deixou a página pré carregada para mim.
           */}
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
