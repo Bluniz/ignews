1cd12a1eebc012e6e039c1cb6625006c08b68a5c
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-06-14 14:16:00 -0300

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   //? Pastas para ignorar, ou seja não vai procurar quando for fazer testes
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   //! Um array de arquivos que jest deve executar antes dos testes
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTestes.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   //? Para arquivos com ts, converta de forma que o jest entenda.
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_Modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
   //? Em que ambiente nossos testes estão executando
   testEnvironment: "jsdom",
